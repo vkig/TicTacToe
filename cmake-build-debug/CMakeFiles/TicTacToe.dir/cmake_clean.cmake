64196c7a1ecd353eabaf7d7f7033f1da5bb9f2dd
vkig <pakozdi.gergo@gmail.com>
2021-05-05 14:52:36 +0200

    First empty project

---
+++ cmake-build-debug/CMakeFiles/TicTacToe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TicTacToe.dir/main.cpp.obj"
+  "TicTacToe.exe"
+  "TicTacToe.exe.manifest"
+  "TicTacToe.pdb"
+  "libTicTacToe.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TicTacToe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
