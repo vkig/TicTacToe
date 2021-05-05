962278c51717ae806446a8d092631059af27152c
vkig <pakozdi.gergo@gmail.com>
2021-05-05 16:02:48 +0200

    First empty project

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include "graphics.hpp"
+
+using namespace std;
+using namespace genv;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    gout.open(200, 200);
+    gout<<move_to(10,10)<<box_to(50, 50)<<refresh;
+    event ev;
+    while(gin>>ev){}
     return 0;
 }
